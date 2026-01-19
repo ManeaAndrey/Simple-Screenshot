65e6d1cd5aa2e58107ef8c7c9dfb0cfa342c2cd8
camanea <catalin-andrei.manea@ubisoft.com>
2026-01-19 11:07:31 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Simple_Screenshot
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SummaryAssistActivator = new Button();
+            textBox1 = new TextBox();
+            SummaryLabel = new Label();
+            Prereqs = new Label();
+            PrereqButton = new Button();
+            PCPrereqs = new CheckedListBox();
+            SuspendLayout();
+            // 
+            // SummaryAssistActivator
+            // 
+            SummaryAssistActivator.Location = new Point(238, 76);
+            SummaryAssistActivator.Name = "SummaryAssistActivator";
+            SummaryAssistActivator.Size = new Size(126, 23);
+            SummaryAssistActivator.TabIndex = 3;
+            SummaryAssistActivator.Text = "Add Summary";
+            SummaryAssistActivator.UseVisualStyleBackColor = true;
+            SummaryAssistActivator.Click += SummaryAssistActivatorButton1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(87, 77);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(145, 23);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // SummaryLabel
+            // 
+            SummaryLabel.AutoSize = true;
+            SummaryLabel.Location = new Point(23, 81);
+            SummaryLabel.Name = "SummaryLabel";
+            SummaryLabel.Size = new Size(58, 15);
+            SummaryLabel.TabIndex = 7;
+            SummaryLabel.Text = "Summary";
+            SummaryLabel.Click += label1_Click;
+            // 
+            // Prereqs
+            // 
+            Prereqs.AutoSize = true;
+            Prereqs.Location = new Point(23, 125);
+            Prereqs.Name = "Prereqs";
+            Prereqs.Size = new Size(46, 15);
+            Prereqs.TabIndex = 8;
+            Prereqs.Text = "Prereqs";
+            Prereqs.Click += label1_Click_1;
+            // 
+            // PrereqButton
+            // 
+            PrereqButton.Location = new Point(238, 121);
+            PrereqButton.Name = "PrereqButton";
+            PrereqButton.Size = new Size(126, 23);
+            PrereqButton.TabIndex = 13;
+            PrereqButton.Text = "Add Prereq";
+            PrereqButton.UseVisualStyleBackColor = true;
+            PrereqButton.Click += PrereqButton_Click;
+            // 
+            // PCPrereqs
+            // 
+            PCPrereqs.FormattingEnabled = true;
+            PCPrereqs.Items.AddRange(new object[] { "PC-Main-Raw", "PC-Main-Final", "PC-TU-Raw", "PC-TU-Final" });
+            PCPrereqs.Location = new Point(87, 125);
+            PCPrereqs.Name = "PCPrereqs";
+            PCPrereqs.Size = new Size(145, 94);
+            PCPrereqs.TabIndex = 14;
+            PCPrereqs.SelectedIndexChanged += checkedListBox1_PCPrereqs;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(PCPrereqs);
+            Controls.Add(PrereqButton);
+            Controls.Add(Prereqs);
+            Controls.Add(SummaryLabel);
+            Controls.Add(textBox1);
+            Controls.Add(SummaryAssistActivator);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button SummaryAssistActivator;
+        private TextBox textBox1;
+        private Label SummaryLabel;
+        private Label Prereqs;
+        private Button PrereqButton;
+        private CheckedListBox PCPrereqs;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Simple_Screenshot
+{
+    public partial class Form1 : Form
+    {
+        private const int HOTKEY_ID = 1;
+        private const int WM_HOTKEY = 0x0312;
+        private const uint MOD_CONTROL = 0x0002;
+        private const uint MOD_SHIFT = 0x0004;
+
+        string? SummaryAssist;
+        int[] Prereq;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+            FormClosing += Form1_FormClosing;
+        }
+
+        private void Form1_Load(object? sender, EventArgs e)
+        {
+
+            var ok = RegisterHotKey(Handle, HOTKEY_ID, MOD_CONTROL | MOD_SHIFT, (uint)Keys.J);
+            if (!ok)
+            {
+                MessageBox.Show("Failed to register hotkey Ctrl+Shift+J", Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Form1_FormClosing(object? sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(Handle, HOTKEY_ID);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY && m.WParam.ToInt32() == HOTKEY_ID)
+            {
+                try
+                {
+                    TakeScreenshot();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error taking screenshot: {ex.Message}", Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void TakeScreenshot()
+        {
+            var bounds = SystemInformation.VirtualScreen;
+            using var bitmap = new Bitmap(bounds.Width, bounds.Height, PixelFormat.Format32bppArgb);
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.CopyFromScreen(bounds.Left, bounds.Top, 0, 0, bounds.Size, CopyPixelOperation.SourceCopy);
+            }
+
+            var pictures = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            var dir = Path.Combine(pictures, "SimpleScreenshots");
+            Directory.CreateDirectory(dir);
+            var file = Path.Combine(dir, $"screenshot_{DateTime.Now:yyyyMMdd_HHmmss}.png");
+            bitmap.Save(file, ImageFormat.Png);
+
+
+            var ni = new NotifyIcon
+            {
+                Visible = true,
+                Icon = SystemIcons.Application,
+                BalloonTipTitle = "Screenshot saved",
+                BalloonTipText = file
+            };
+            ni.ShowBalloonTip(3000);
+
+            var cleanupTimer = new System.Windows.Forms.Timer { Interval = 4000 };
+            cleanupTimer.Tick += (s, e) =>
+            {
+                cleanupTimer.Stop();
+                cleanupTimer.Dispose();
+                ni.Dispose();
+            };
+            cleanupTimer.Start();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            SummaryAssist = textBox1.Text;
+
+
+
+        }
+
+        private void SummaryAssistActivatorButton1_Click(object sender, EventArgs e)
+        {
+            var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var dir = Path.Combine(documents, "Summary");
+            File.WriteAllText(dir, SummaryAssist);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_PCPrereqs(object sender, EventArgs e)
+        {
+            Prereq = PCPrereqs.CheckedIndices.Cast<int>().ToArray();
+
+
+        }
+        private void PrereqButton_Click(object sender, EventArgs e)
+        {
+           
+            var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var dir = Path.Combine(documents, "Prereqs");
+            
+            foreach (int index in Prereq)
+            {
+               
+                File.WriteAllText(dir, PCPrereqs.Items[index].ToString());
+              
+            }
+
+
+        }
+
+      
+    }
+}
